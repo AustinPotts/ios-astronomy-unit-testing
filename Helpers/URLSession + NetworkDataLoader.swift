32908ca98344f499f22b76a4b2d8b18acb6758c6
AustinPotts <pottsevolvements@gmail.com>
2020-02-25 11:01:58 -0500

    Cl

--- Helpers/URLSession + NetworkDataLoader.swift
+++ Helpers/URLSession + NetworkDataLoader.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 extension URLSession: NetworkDataLoader {
-    func loadData(from request: URLRequest, completion: @escaping NetworkCompletion) {
-        dataTask(with: request) { data, response, error in
-            completion(data, response as? HTTPURLResponse, error)
-        }.resume()
-    }
+//    func loadData(from request: URLRequest, completion: @escaping NetworkCompletion) {
+//        dataTask(with: request) { data, response, error in
+//            completion(data, response as? HTTPURLResponse, error)
+//        }.resume()
+//    }
     
     func loadData(from url: URL, completion: @escaping (Data?, HTTPURLResponse?, Error?) -> ()) {
         dataTask(with: url) { data, response, error in
